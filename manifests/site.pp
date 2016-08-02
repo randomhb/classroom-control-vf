5621d74283d69dd937d11200db0a772d8949aa64
randomhb <randomhb@users.noreply.github.com>
2016-08-02 13:21:10 -0400

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -43,5 +43,5 @@ node default {
   # Example:
   #   class { 'my_class': }
   notify { "Hello, my name is ${::hostname}": }
-  notify { "This is just a demo.": }
+  notify { "Hello, from Florida!": }
 }
