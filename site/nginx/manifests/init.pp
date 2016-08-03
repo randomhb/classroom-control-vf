240c90592e15a9a1ad37ecd8c82d3106da59bb87
randomhb <randomhb@users.noreply.github.com>
2016-08-03 14:40:07 -0400

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,15 +1,43 @@
 class nginx {
 
-  file { '':
-  
+  file { '/etc/nginx/conf/default.conf':
+    ensure => present,
+    source => 'puppet:///modules/nginx/default.conf',
+    owner => 'root',
+    group => 'root',
+    mode => '0644',
+    path => '/etc/nginx/conf/default.conf'
+    require => Package['nginx'],
   }
   
-  service { '':
-  
+  file { '/etc/nginx/conf/nginx.conf':
+    ensure => present,
+    source => 'puppet:///modules/nginx/nginx.conf',
+    owner => 'root',
+    group => 'root',
+    mode => '0644',
+    path => '/etc/nginx/conf/nginx.conf'
+    require => Package['nginx'],
   }
   
-  package { '':
+  file { '/etc/nginx/html/index.html':
+    ensure => present,
+    source => 'puppet:///modules/nginx/html/idex.html',
+    owner => 'root',
+    group => 'root',
+    mode => '0644',
+    path => '/etc/nginx/html/index.html'
+    require => Package['nginx'],
+  }
   
+  service { 'nginx':
+    ensure => running,
+    enable => true,
+    require => File['/etc/nginx/conf/nginx.conf'],
+  }
+  
+  package { 'nginx':
+    ensure => present,
   }
   
 }
