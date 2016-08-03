f8998999dba467035edbd26850ac3130f29ec174
randomhb <randomhb@users.noreply.github.com>
2016-08-03 15:16:33 -0400

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -6,44 +6,44 @@ class nginx {
   
   file { '/var/www':
     ensure => 'directory',
-    owner => 'root',
-    group => 'root',
+    owner  => 'root',
+    group  => 'root',
   }
   
   file { 'default.conf':
-    ensure => 'file',
-    source => 'puppet:///modules/nginx/default.conf',
-    owner => 'root',
-    group => 'root',
-    mode => '0644',
-    path => '/etc/nginx/conf/conf.d/default.conf'
-    require   => Package['nginx'],
-    notify    => Service['nginx'],
+    ensure  => 'file',
+    source  => 'puppet:///modules/nginx/default.conf',
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
+    path    => '/etc/nginx/conf/conf.d/default.conf'
+    require => Package['nginx'],
+    notify  => Service['nginx'],
   }
   
   file { 'nginx.conf':
-    ensure => 'file',
-    source => 'puppet:///modules/nginx/nginx.conf',
-    owner => 'root',
-    group => 'root',
-    mode => '0644',
-    path => '/etc/nginx/nginx.conf'
-    require   => Package['nginx'],
-    notify    => Service['nginx'],
+    ensure  => 'file',
+    source  => 'puppet:///modules/nginx/nginx.conf',
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
+    path    => '/etc/nginx/nginx.conf'
+    require => Package['nginx'],
+    notify  => Service['nginx'],
   }
   
   file { 'index.html':
     ensure => 'file',
     source => 'puppet:///modules/nginx/idex.html',
-    owner => 'root',
-    group => 'root',
-    mode => '0644',
-    path => '/var/www/index.html'
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0644',
+    path   => '/var/www/index.html'
   }
   
   service { 'nginx':
-    ensure => running,
-    enable => true,
+    ensure     => running,
+    enable     => true,
     hasrestart => 'true',
   }
   
