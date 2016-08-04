9b8b37102f70bbab538d6b2d251e2feffdded23d
randomhb <randomhb@users.noreply.github.com>
2016-08-04 15:07:23 -0400

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,8 +1,57 @@
 class nginx {
+  case $::os['family'] {
+    'redhat', 'debian' : {
+      $owner    = 'root'
+      $group    = 'root'
+      $package  = 'nginx'
+      $docroot  = '/var/www'
+      $confdir  = '/etc/nginx'
+      $blockdir = "${confdir}/conf.d"
+      $logdir   = '/var/log/nginx'
+    }
+    'windows' : {
+      $owner    = 'Administrator'
+      $group    = 'Administrators'
+      $package  = 'nginx'
+      $docroot  = 'C:/ProgramData/nginx/html'
+      $confdir  = 'C:/ProgramData/nginx'
+      $blockdir = "${confdir}/conf.d"
+      $logdir   = "${confdir}/logs"
+    }
+    default : {
+      fail("Module ${module_name} is not supported on ${os['family']}")
+    }
+  }
 
-  package { 'nginx':
+  $user = $::os['family'] ? {
+    'redhat'  => 'nginx',
+    'debian'  => 'www-data',
+    'windows' => 'nobody',
+  }
+  
+  package { $package:
     ensure => present,
   }
+  ################################
+  
+  $user = $::os[;family'] ? {
+    'redhat' => 'nginx',
+    'debian' => 'www-data',
+    'windows' => 'nobody',
+  }
+  
+  file { '$docroot':
+    ensure => 'directory',
+  }
+  
+  
+  
+  
+  
+  
+  
+  
+  #################################
   
   File {
     owner => 'root',
