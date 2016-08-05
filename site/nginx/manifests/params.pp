9c20885a7c440669cd88c015713dfa6e005c56bc
randomhb <randomhb@users.noreply.github.com>
2016-08-05 10:54:55 -0400

    Update params.pp

--- site/nginx/manifests/params.pp
+++ site/nginx/manifests/params.pp
@@ -1,1 +1,25 @@
 class nginx::params {
+  case $::os['family'] {
+    'redhat', 'debian' : {
+      $owner    = 'root'
+      $group    = 'root'
+      $package  = 'nginx'
+      $docroot  = '/var/www/'
+      $confdir  = '/etc/nginx/'
+      $servblock = "${confdir}/conf.d/"
+      $logpath   = '/var/log/nginx/'
+    }
+    'windows' : {
+      $owner    = 'Administrator'
+      $group    = 'Administrators'
+      $package  = 'nginx'
+      $docroot  = 'C:/ProgramData/nginx/html/'
+      $confdir  = 'C:/ProgramData/nginx/'
+      $servblock = "${confdir}/conf.d/"
+      $logpath   = "${confdir}/logs/"
+      }
+      default : {
+        fail("Module ${module_name} is not supported on ${::os['family']}")
+      }
+    }
+}
