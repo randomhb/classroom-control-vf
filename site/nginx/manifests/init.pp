6fb2193011f4a92003d961a5952724b93f4e1057
randomhb <randomhb@users.noreply.github.com>
2016-08-05 11:04:49 -0400

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -2,12 +2,11 @@ class nginx(
    $package = $nginx::params::package,
    $owner = $nginx::params::owner,
    $group = $nginx::params::group,
-   $root_dir = $nginx::params::root_dir,
-   $document_dir = $nginx::params::document_dir,
-   $conf_dir = $nginx::params::conf_dir,
-   $log_dir = $nginx::params::log_dir,
-   $service_name = $nginx::params::service_name,
-   $service_user = $nginx::params::service_user,
+   $docroot = $nginx::params::docroot,
+   $servblock = $nginx::params::servblock,
+   $confdir = $nginx::params::confdir,
+   $logpath = $nginx::params::logpath,
+   $user = $nginx::params::_user,
    )
   inherits nginx::params {
   
